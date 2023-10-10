e79e456f35bd7cfec77b1aaaa41a99fe701a5b09
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2023-10-10 08:58:14 +0300

    Fix navigation bug

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -37,14 +37,20 @@ const NavBar: React.FC<NavBarPropsInterfce> = (props) => {
     setSearch(value);
   };
 
-  const onNowPlayingClick = () =>
+  const onNowPlayingClick = () => {
     props.setgqlQuery({ query: GET_NOW_PLAYING, type: GET_NOW_PLAYING_TYPE });
+    navigate('/');
+  };
 
-  const onPopularClick = () =>
+  const onPopularClick = () => {
     props.setgqlQuery({ query: GET_POPULAR, type: GET_POPULAR_TYPE });
+    navigate('/');
+  };
 
-  const onTopRatedClick = () =>
+  const onTopRatedClick = () => {
     props.setgqlQuery({ query: GET_TOP_RATED, type: GET_TOP_RATED_TYPE });
+    navigate('/');
+  };
 
   const onSearchClick = () => {
     props.setgqlQuery({ query: SEARCH_MOVIE, type: SEARCH_MOVIE_TYPE });

4398e33e2ab9dfe167c21f56a397d9add7354b54
DESKTOP-5015UJ0\Roman <rdmytruk78@gmail.com>
2023-10-15 12:14:05 +0300

    Add snapshot for MoviePopUp

---
+++ client/src/tests/components/MoviePopUp.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { MockedProvider } from '@apollo/client/testing';
+import '@testing-library/jest-dom';
+import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import { MoviePopUp } from '../../components/index';
+
+import { mocks } from '../mocks';
+import { moviesReducer } from '../../redux/ducks/movies';
+import { detailMovies } from '../../util/Data/testDataDetailMovies';
+
+const store = createStore(moviesReducer, detailMovies);
+
+test('should render App component and match snapshot', async () => {
+  const { asFragment } = render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <Provider store={store}>
+        <MemoryRouter>
+          <MoviePopUp isShow={true} />
+        </MemoryRouter>
+      </Provider>
+    </MockedProvider>
+  );
+
+  expect(asFragment()).toMatchSnapshot();
+});
